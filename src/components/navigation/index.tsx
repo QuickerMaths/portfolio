131c9de361f8e30e0896b994ef8eded5774b001a
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-02-29 19:47:12 +0100

    Add theme provider, theme switch, navigation, and button components

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import React from 'react'
+import Link from 'next/link'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu"
+import { ThemeSwitch } from "@/components/theme-switch"
+
+
+const Navigation = () => {
+  return (
+    <div className="w-full py-5 px-10 flex justify-between items-center">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Projects
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Contact
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <ThemeSwitch />
+    </div>
+  )
+}
+
+export default Navigation
