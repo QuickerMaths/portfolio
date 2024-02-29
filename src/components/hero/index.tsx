9a8816f405d992c2d06530374ba99e9d238718d4
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-02-29 23:41:21 +0100

    Add social media icons to hero section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
 import profileImage from '../../assets/images/mateusz-szalowicz.jpeg'
+import { Button } from '../ui/button'
+import { GitHubLogoIcon, LinkedInLogoIcon, EnvelopeClosedIcon } from "@radix-ui/react-icons"
 
 const Hero = () => {
   return (
@@ -11,6 +13,23 @@ const Hero = () => {
             <p className="text-sm text-gray-400 dark:text-gray-500">
                 I am an aspiring web developer with a deep passion for crafting seamless and visually appealing web experiences.
             </p>
+            <div className='flex items-center gap-2 text-3xl'>
+              <Button variant="ghost" size="icon" asChild>
+                <a href="https://github.com/QuickerMaths" target='_blank'>
+                  <GitHubLogoIcon />
+                </a>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <a href="https://linkedin.com/matuesz.szalowicz" target='_blank'>
+                  <LinkedInLogoIcon />
+                </a>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <a href="mailto:mateusz.szalowicz@gmail.com" target='_blank'>
+                  <EnvelopeClosedIcon />
+                </a>
+              </Button>
+            </div>
         </div>
         <Image
           src={profileImage.src}
