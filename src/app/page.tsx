a0b56a6b2302210cd598a42090cea1221baf2e8f
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-03-02 00:48:51 +0100

    Refactor page component to add TechStack component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/hero/index'
+import TechStack from '@/components/tech-stack';
 
-export default function Home() {
+export default function home() {
   return (
-    <main className="min-h-screen content-container flex flex-col items-center py-10 px-5">
+    <main className="min-h-screen content-container flex flex-col justify-center items-center py-10 px-5">
       <Hero />
+      <TechStack />
     </main>
   );
 }
