18fa1dec22c7977a5cfe44ff3fb69c62aae6cfb4
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-03-12 00:01:44 +0100

    Add new files and components for project page

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import ProjectAbout from "@/components/project-about";
+import { notFound } from "next/navigation";
+import { allProjects } from "contentlayer/generated";
+import { Mdx } from "@/components/mdx";
+import "../../../css/mdx.css"
+import { Separator } from "@/components/ui/separator";
+
+export const revalidate = 60;
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateStaticParams(): Promise<Props["params"][]> {
+  return allProjects
+    .filter((p) => p.published)
+    .map((p) => ({
+      slug: p.slug,
+    }));
+}
+
+export default async function PostPage({ params }: Props) {
+  const slug = params?.slug;
+  const project = allProjects.find((project) => project.slug === slug);
+
+  if (!project) {
+    notFound();
+  }
+
+  return (
+    <div className="min-h-screen content-container flex flex-col justify-center items-center bg-background py-10">
+      <ProjectAbout project={project} />
+      <Separator className="w-[80%] my-4 mx-auto bg-primary" />
+      <article className="px-4 py-12 mx-auto content-container">
+        <h2 className="text-xl text-primary font-semibold md:text-3xl text-center mb-5 underline">
+          About the project
+        </h2>
+        <Mdx code={project.body.code} />
+      </article>
+    </div>
+  );
+}
---
+++ src/components/project-about/index.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import React from 'react'
+
+interface ProjectAboutProps {
+	project: {
+		url?: string;
+		title: string;
+		description: string;
+		repository?: string;
+    }
+}
+
+const ProjectAbout = ({project}: ProjectAboutProps) => {
+  const links: { label: string; href: string }[] = [];
+
+  if (project.repository) {
+		links.push({
+			label: "GitHub",
+			href: `https://github.com/${project.repository}`,
+		});
+	}
+
+	if (project.url) {
+		links.push({
+			label: "Website",
+			href: project.url,
+		});
+	}
+
+  return (
+    <section className="w-full flex flex-col justify-center items-center gap-5 px-5 md:px-0">
+      <h1 className="text-primary text-3xl md:text-5xl font-bold text-center">
+        {project.title}
+      </h1>
+      <p className='text-secondary text-center'>
+        {project.description}
+      </p>
+      <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+						<div className="grid grid-cols-1 gap-y-2 gap-x-8 leading-7 sm:grid-cols-2 md:flex lg:gap-x-10">
+							{links.map((link) => (
+								<a target="_blank" key={link.label} href={link.href} className='text-secondary hover:text-primary duration-200 font-semibold'>
+									{link.label} <span aria-hidden="true">&rarr;</span>
+								</a>
+							))}
+						</div>
+					</div>
+    </section>
+  )
+}
+
+export default ProjectAbout
