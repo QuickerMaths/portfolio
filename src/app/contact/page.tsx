44be31f63c821f6dcab35d2118a9dad06a851e84
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-03-16 16:34:01 +0100

    Update contact page layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from '@/components/contact-form'
 
 const Contact = () => {
   return (
-    <div className='w-full min-h-screen flex justify-center items-center gap-10'>
+    <main className='w-full min-h-screen flex justify-center items-center gap-10'>
         <ContactForm />
         <div className='hidden h-full md:flex flex-col justify-center'>
           <h1 className='relative mb-20 font-bold uppercase text-5xl text-secondary tracking-widest after:content-"" after:bg-primary after:dark:bg-primary after:absolute after:-bottom-5 after:left-0 after:w-1/2 after:h-[3px]'>
@@ -14,7 +14,7 @@ const Contact = () => {
             <p>or send me an email at <a className='underline text-primary' href='mailto:matuesz.szalowicz@gmail.com'>mateusz.szalowicz@gmail.com</a></p>
           </div>
         </div>
-    </div>
+    </main>
   )
 }
 
