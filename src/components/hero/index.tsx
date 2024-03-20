adac6b167f808b345a189e39d4e8ce053b83008d
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-03-20 13:50:16 +0100

    Fix typo and update hero description

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -8,10 +8,10 @@ const Hero = () => {
   return (
     <div className="mx-auto flex flex-col-reverse sm:flex-row items-center gap-5 max-w-[480px] sm:max-w-[600px]">
         <div className="flex flex-col justify-center gap-2">
-            <h1 className="text-3xl min-[340px]:text-4xl min-[440px]:text-5xl font-serif text-primary">Matuesz Szałowicz</h1>
+            <h1 className="text-3xl min-[340px]:text-4xl min-[440px]:text-5xl font-serif text-primary">Mateusz Szałowicz</h1>
             <h2 className='text-secondary dark:text-secondary font-semibold'>Web developer</h2>
             <p className="text-sm text-gray-700 dark:text-gray-400">
-                I am an aspiring web developer with a deep passion for crafting seamless and visually appealing web experiences.
+              I'm a dedicated developer with a passion for crafting visually stunning and highly functional websites. Explore my work to see how I can bring your digital vision to life. Let's build something extraordinary together.
             </p>
             <div className='flex items-center gap-2 text-3xl'>
               <Button variant="ghost" size="icon" asChild>
